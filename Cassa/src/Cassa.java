ea4c04643ee391eecfa8cf14151e9b798c0ba97d
MichealPlug <37807751+MichealPlug@users.noreply.github.com>
2019-03-25 17:14:32 +0100

    Update Cassa.java

--- Cassa/src/Cassa.java
+++ Cassa/src/Cassa.java
@@ -1,4 +1,23 @@
-
-public class Cassa {
-
+public class Cassa{	
+	
+	private int prezzoDiVendita;
+	private int importoPagato;
+	
+	public Cassa (int prezzoDiVendita) {
+		this.prezzoDiVendita = prezzoDiVendita;
+		importoPagato = 0;
+	}
+	
+	public int pagamento (int importoPagato) {
+		this.importoPagato += importoPagato;
+		int resto = this.importoPagato - prezzoDiVendita;	
+		stampaRisultato(resto);	
+		return resto;
+	}	
+	
+	public void stampaRisultato (int resto) {
+		if (resto==0) System.out.println("Transazione completata");
+		else if (resto>0) System.out.println("Transazione completata, resto: "+ resto +"EUR");
+		else System.out.println("Pagamento insufficente, debito di "+ (-resto)+"EUR");
+	}
 }
